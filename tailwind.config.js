38017c727ae5f77119ba167b0a4a4ce8a8914a1a
Smarta Dey <worldsmarta@gmail.com>
2021-12-05 11:25:11 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "ubl1-col": "#733fc8",
+        "ubl2-col": "#49556b",
+        "lbl1-col": "#ffffff",
+        "lbl2-col": "#18202d"
+
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
